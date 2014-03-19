b0c498e8815a2a38a658a79faf58ce257dba8a98
Paul Belanger <paul.belanger@polybeacon.com>
2014-03-19 01:56:17 +0000

    Install pip using get-pip
    Use get-pip to install pip per:
      http://www.pip-installer.org/en/latest/installing.html
    
    This is because the system version of pip is not new enough for some
    python packages.
    
    Change-Id: I142eb8f75e566d209be2bbc8fa4d4b53953de49f
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- tools/install_puppet.sh
+++ tools/install_puppet.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+
+# Copyright 2013 OpenStack Foundation.
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+# Copyright 2013 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Install pip using get-pip
+EZ_SETUP_URL=https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+PIP_GET_PIP_URL=https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+
+curl -O $EZ_SETUP_URL || wget $EZ_SETUP_URL
+python ez_setup.py
+curl -O $PIP_GET_PIP_URL || wget $PIP_GET_PIP_URL
+python get-pip.py
+
 cat > 00-puppet.pref <<EOF
 Package: puppet puppet-common puppetmaster puppetmaster-common puppetmaster-passenger
 Pin: version 2.7*
