7ea068a2ae23e249afff63c68e317dbe2e8ff0be
Paul Belanger <paul.belanger@polybeacon.com>
2014-03-29 16:47:32 -0400

    Create gruntrc file
    This will be the home of all our grunt specific configuration variables.
    
    Change-Id: Ic866cf05cf8b8f8c4527766a7e6c83800750aad9
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- grunt.sh
+++ grunt.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+source gruntrc
+
+PUPPET_ARGS=""
+
+if [[ "$GRUNT_DEBUG" == "True" ]]; then
+    PUPPET_ARGS+="--debug"
+fi
 
 if [ ! -f /etc/apt/sources.list.d/puppetlabs.list ]; then
     ./tools/install_puppet.sh
@@ -8,4 +16,4 @@ if [ ! -d .modules ]; then
     ./tools/install_modules.sh
 fi
 
-sudo puppet apply --verbose --modulepath='modules:.modules' manifests/site.pp --certname=grunt
+sudo puppet apply --verbose --modulepath='modules:.modules' manifests/site.pp --certname=grunt $PUPPET_ARGS
