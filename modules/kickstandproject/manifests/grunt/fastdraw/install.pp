e6264e0d2fed810daf896b8b19c5e1e6989d6532
Paul Belanger <paul.belanger@polybeacon.com>
2014-04-05 17:53:35 -0400

    Add support for fastdraw
    Change-Id: I6f3562d3aa330b1b73bd008383161d85d25b4611
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ modules/kickstandproject/manifests/grunt/fastdraw/install.pp
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2014, PolyBeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+class kickstandproject::grunt::fastdraw::install(
+  $path = '/opt/kickstandproject/fastdraw',
+  $revision = 'master',
+  $source = 'https://github.com/kickstandproject/fastdraw.git',
+) {
+  vcsrepo { $path:
+    ensure   => latest,
+    notify   => Exec["pip install -e ${path}"],
+    provider => git,
+    revision => $revision,
+    source   => $source,
+  }
+
+  exec { "pip install -e ${path}":
+    refreshonly => true,
+    require     => Package['python-pip'],
+  }
+}
+
+# vim:sw=2:ts=2:expandtab
