8fe6e1a88fd71ba732e1aeda5ee2d33dd496e467
Paul Belanger <paul.belanger@polybeacon.com>
2014-05-01 15:15:06 +0000

    Initial skeleton python application
    We'll actually need to run smoke tests for grunt, to actually
    ensure the functionality of Puppet works.  This is the base
    framework for it.
    
    Change-Id: I21a2858e8b8d6384d59c4f7e7c52cc7f6638a1f2
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ doc/source/conf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013 PolyBeacon, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Grunt'
+copyright = u'2013, Paul Belanger'
+pygments_style = 'sphinx'
+html_theme = 'default'
+htmlhelp_basename = 'gruntedoc'
+
+latex_elements = {
+}
+
+latex_documents = [
+    ('index', 'grunt.tex', u'Grunt Documentation',
+     u'Paul Belanger', 'manual'),
+]
+
+texinfo_documents = [
+    ('index', 'grunt', u'Grunt Documentation',
+     u'Paul Belanger', 'grunt', 'One line description of project.',
+     'Miscellaneous'),
+]
