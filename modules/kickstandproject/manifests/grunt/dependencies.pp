142f4d40d030d0145559ac0ea8fd92b620e263ad
Paul Belanger <paul.belanger@polybeacon.com>
2014-10-03 13:15:02 -0400

    Unpin Requests
    As the upstream bug as been resolved.
    
    Change-Id: I75fe5a444f060ee7a6a25b4c9cfcc38a2e6b04dd
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/grunt/dependencies.pp
+++ modules/kickstandproject/manifests/grunt/dependencies.pp
@@ -16,15 +16,6 @@ class kickstandproject::grunt::dependencies {
   package { $packages:
     ensure => present,
   }
-
-  # NOTE(pabelanger): Because of a bug with python-keystoneclient we need to
-  # pin requests to 2.3.0. Besure to remove this code when
-  # https://bugs.launchpad.net/python-keystoneclient/+bug/1363179 is resolved.
-  package { 'requests':
-    ensure   => '2.3.0',
-    provider => pip,
-    require  => Package[$packages],
-  }
 }
 
 # vim:sw=2:ts=2:expandtab
