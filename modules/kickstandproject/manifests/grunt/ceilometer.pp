e45e3cd5e4b53f8860de1f45143affa6943d0aad
Paul Belanger <paul.belanger@polybeacon.com>
2015-01-05 20:10:41 -0500

    Initial commit for ceilometer
    Change-Id: I0af48d73dc3d800c12384394e13c34ca75fc4895
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ modules/kickstandproject/manifests/grunt/ceilometer.pp
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2014, PolyBeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+class kickstandproject::grunt::ceilometer(
+  $db_host = $::database_host,
+  $db_name = $::ceilometer_db_name,
+  $db_password = $::ceilometer_db_password,
+  $db_type = $::database_type,
+  $db_user = $::ceilometer_db_user,
+) {
+
+  $sql_conn = "${db_type}://${db_user}:${db_password}@${db_host}/${db_name}"
+
+  if ($db_type == 'mysql') {
+    class { '::ceilometer::db::mysql':
+      dbname   => $db_name,
+      host     => $db_host,
+      password => $db_password,
+      user     => $db_user,
+    }->
+  } else {
+    class { '::ceilometer::db::postgresql':
+      dbname   => $db_name,
+      password => $db_password,
+      user     => $db_user,
+    }->
+  }
+  class { '::ceilometer::db':
+    database_connection => $sql_conn,
+  }->
+  class { '::ceilometer::keystone::auth':
+    admin_address    => $::ipaddress,
+    internal_address => $::ipaddress,
+    public_address   => $::ipaddress,
+  }->
+  class { '::ceilometer::collector': }
+}
+
+# vim:sw=2:ts=2:expandtab
