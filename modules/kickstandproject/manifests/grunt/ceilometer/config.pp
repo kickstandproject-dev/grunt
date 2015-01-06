5703574bd5b6a76b7fcd1d2a24e6c856db687ced
Paul Belanger <paul.belanger@polybeacon.com>
2015-01-05 20:10:41 -0500

    Initial commit for ceilometer
    Change-Id: I0af48d73dc3d800c12384394e13c34ca75fc4895
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ modules/kickstandproject/manifests/grunt/ceilometer/config.pp
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2014, PolyBeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+class kickstandproject::grunt::ceilometer::config {
+  class { '::ceilometer':
+    metering_secret => 'secret',
+  }->
+  class { '::ceilometer::keystone::auth':
+    admin_address    => $::ipaddress,
+    internal_address => $::ipaddress,
+    password         => 'ceilometer',
+    public_address   => $::ipaddress,
+  }->
+  class { '::ceilometer::collector': }
+}
+
+# vim:sw=2:ts=2:expandtab
